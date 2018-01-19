51de2704e49439ddc7cad8b38bf97b99c05c8ad6
Admin123 <minhngocvu1612@gmail.com>
2018-01-19 15:25:25 +0700

    update

--- resources/views/admin/layout/index.blade.php
+++ resources/views/admin/layout/index.blade.php
@@ -9,16 +9,12 @@
     <base href="{{asset('')}}">
     <!-- Bootstrap Core CSS -->
     <link href="admin_asset/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom CSS -->
     <link href="admin_asset/dist/css/sb-admin-2.css" rel="stylesheet">
-
     <!-- Custom Fonts -->
     <link href="admin_asset/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
     <!-- DataTables CSS -->
     <link href="admin_asset/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
-
     <!-- DataTables Responsive CSS -->
     <link href="admin_asset/bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
 
@@ -47,16 +43,16 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="{{asset('/js/jquery-ui.js')}}"></script>
-    <script src="{{asset('/js/jquery-ui.min.js')}}"></script>
-
+  
+    <script src="{{asset('js/jquery.js')}}"></script>
     <script src="{{asset('admin_asset/bower_components/jquery/dist/jquery.min.js')}}"></script>
     <!-- Bootstrap Core JavaScript -->
     <script src="{{asset('admin_asset/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
-
+    <script src="{{asset('js/jquery-ui.js')}}"></script>
+    <script src="{{asset('js/jquery-ui.min.js')}}"></script>
     @yield('script')
 </body>
 
