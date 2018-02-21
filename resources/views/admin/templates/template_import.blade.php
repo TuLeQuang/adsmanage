777a7af6c375256fa500fa83de6518458323158d
LeTu <lequangtu28@gmail.com>
2018-02-21 15:48:47 +0700

    update select key to render form

--- resources/views/admin/templates/template_import.blade.php
+++ resources/views/admin/templates/template_import.blade.php
@@ -2,7 +2,7 @@
 @section('title')
   <title>Import Template</title>
   <meta id="csrf-token" name="csrf-token" value="{{ csrf_token() }}">
-  {{-- <link href="/templatemanager/node_modules/medium-editor/dist/css/medium-editor.css" rel="stylesheet">--}}
+  <link href="{{asset('css/bootstrap-multiselect.css')}}" rel="stylesheet">
 @endsection
 @section('style')
   <style type="text/css">
@@ -22,14 +22,18 @@
            <span id="msg" style="color: red"></span><br>
            <textarea type="text" id="scriptText" class="form-control" rows="10" style="width: 100%"></textarea><br>
             <button id="btn-data" class="btn btn-info" onclick="getData()">Get Form</button>
-            <a href="https://demo.admicro.vn/testscript/" target="_blank"><input id="btn-script" type="submit" class="btn btn-info" onclick="renderAds()" style="margin-left: 25px" value="Run Script"></a>
+            <a href="https://demo.admicro.vn/testscript/" target="_blank"><input id="btn-script" type="submit" class="btn btn-info" style="margin-left: 25px" value="Run Script"></a>
           </div>
-          <div id="template-form" style="width: 70%;float: right">
+          <div id="template-form" style="width: 68%;float: right">
+              <div id="checkBoxList">
+                  <select name="lstStates" multiple id="lstStates" onchange="selectKey()" style="display: none">
+                  </select>
+              </div>
+              <div id="form"></div>
           {{--<div id="run-script" style="width: 50%; float: left;display: inline-block">--}}
           </div>
         </div>
 
-        </div>
         <div id="ads">
         </div>
       </div>
@@ -41,12 +45,10 @@
 @endsection
 
 @section('script')
-  {{--  <script src="{{ asset('js/app.js') }}"></script>--}}
-  {{--<script src="/templatemanager/node_modules/vee-validate/dist/vee-validate.js"></script>
-  <script src="/templatemanager/node_modules/vue/dist/vue.js"></script>
-  <script src="js/index.js"></script>--}}
+  <script src="{{asset('js/bootstrap-multiselect.js')}}"></script>
   <script type="text/javascript">
-    var itemsKey=["image","link","title","domain","titlebox","title 1","desc","descr","content","color","src","slogan","logoBrand","descadv","sf_color",""];
+    //var itemsKey=["image","link","title","domain","titlebox","title 1","desc","descr","content","color","src","slogan","logoBrand","descadv","sf_color",""];
+    var itemsKey=[];
     var key=[];//data key in json
     var data="";
 
@@ -57,45 +59,32 @@
             var adsJson=adsScript.slice(adsScript.indexOf("{"),adsScript.lastIndexOf("}")+1);
             adsJson= JSON.parse(adsJson);
 
-            /*// run with this
-            //document.getElementById('template-form').innerHTML=allInternalObjs(adsJson);
-            console.log(allInternalObjs(adsJson));*/
-
-            /*traverse(adsJson, function (key, value, trail) {
-                document.getElementById('template-form').innerHTML=arguments.join();
-            });*/
-
-            //get data and render form
+            //choose key to render form
             var k=[];
-            (function traverse(o,name) {
-                name=name || "";
+            (function traverse(o) {
                 for (var i in o) {
                     if (o[i] !== null && typeof(o[i])=="object") {
-                        k.push(name + i +'<br>');
                         //going on step down in the object tree!!
-                        traverse(o[i],"&#8195;"+name+i+".");
+                        traverse(o[i]);
                     }
                     else{
-                        for(var j in itemsKey){
-                            if(i==itemsKey[j]) {
-                                k.push(name + i +' : <input type="text" id="'+name +i+'" value="'+o[i]+'" onchange="setNewDataJson(\''+name+i+'\')" style="width: 300px;right:0px" class="form-control input-item"><br>'/*+ ' - ' + o[i]*/);
-                            }
-                            /*else{
-                                k.push(name + i +'<br>'/!*+ ' - ' + o[i]*!/);
-                            }*/
-                        }
-
+                        k.push(i);
                     }
                 }
             })
             (adsJson);
 
-            var div = document.createElement('div');
-            div.innerHTML=k.join(" ");
-            document.getElementById('template-form').appendChild(div);
+            //remove key
+            key = remove_duplicates(k);
+            checkBoxList(key);
+            //console.log(key);
+
+            /*var div = document.createElement('div');
+            div.innerHTML=key.join(" ");
+            document.getElementById('template-form').appendChild(div);*/
 
 
-            //get data and data key from script
+           /* //get data and data key from script
             for (var i in adsJson.data) {
                 data +=  JSON.stringify(adsJson.data[i]);
                 if(data.indexOf("items")>0){
@@ -116,12 +105,11 @@
                 }
             }
             //console.log(data);
-            //console.log(key.toString());
+            //console.log(key.toString());*/
         }
         catch(err) {
             document.getElementById("msg").innerHTML = err.message;
         }
-
       };
 
       function renderForm() {
@@ -240,41 +228,88 @@
           document.getElementById('scriptText').value=adsScript.replace(adsScript.slice(adsScript.indexOf("{"),adsScript.lastIndexOf("}")+1),JSON.stringify(adsJson));
       }
 
-   /* // get keys of an object or array
-    function getkeys(z){
-        var out=[];
-        for(var i in z){out.push(i)};
-        return out;
-    }
-    // print all inside an object
-    function allInternalObjs(data,name) {
-        name=name || "";
-        return getkeys(data).reduce(function(olist, k){
-            var v = data[k];
-            if(typeof v === 'object') {
-                olist.push.apply(olist, allInternalObjs(v,name +"."+ k));
-            }
-            else {
-                olist.push(name +"."+k);
-            }
-            return olist;
-        }, []);
+    //render checkbox list form array
+    function checkBoxList(aray) {
+        var checkList=document.getElementById('lstStates');
+        checkList.style.display="block";
+        var f=document.createDocumentFragment();
+        for(var i in aray){
+            var op= document.createElement('option');
+            op.value=aray[i].toString();
+            op.textContent=aray[i].toString();
+            f.appendChild(op);
+        }
+        checkList.appendChild(f);
+        selectKey();
     }
 
-    //cach 2
-    function traverse (obj, callback, trail) {
-        trail = trail || [];
+    //select key and render form
+    function selectKey() {
+          itemsKey=[];
+        $('#lstStates').multiselect({
+            buttonText: function(options, select) {
+                options.each(function() {
+                    itemsKey.push($(this).val());
+                });
+                //console.log(itemsKey+1);
+                renderForm2();
+                if (options.length === 0) {
+                    return 'None selected';
+                }
+                if (options.length === select[0].length) {
+                    return 'All selected ('+select[0].length+')';
+                }
+                else if (options.length >= 4) {
+                    return options.length + ' selected';
+                }
+                else {
+                    var labels = [];
+                    options.each(function() {
+                        labels.push($(this).val());
+                    });
+                    return labels.join(', ') + '';
+                }
+            },
+        });
 
-        Object.keys(obj).forEach(function (key) {
-            var value = obj[key]
+    };
 
-            if (Object.getPrototypeOf(value) === Object.prototype) {
-                traverse(value, callback, trail.concat(key))
-            } else {
-                callback.call(obj, key, value, trail)
-            }
-        })
-    }*/
+   //render form 2
+    function renderForm2() {
+        //console.log(itemsKey.join(" ")+2);
+        var adsScript=document.getElementById('scriptText').value;
+        var adsJson=adsScript.slice(adsScript.indexOf("{"),adsScript.lastIndexOf("}")+1);
+        adsJson= JSON.parse(adsJson);
+        //get data and render form
+          var k=[];
+          (function traverse(o,name) {
+              name=name || "";
+              for (var i in o) {
+                  if (o[i] !== null && typeof(o[i])=="object") {
+                      //k.push(name + i +'<br>');
+                      //going on step down in the object tree!!
+                      traverse(o[i],"&#8195;"+name+i+".");
+                  }
+                  else{
+                      for(var j in itemsKey){
+                          if(i==itemsKey[j]) {
+                              k.push(name + i +' : <input type="text" id="'+name +i+'" value="'+o[i]+'" onchange="setNewDataJson(\''+name+i+'\')" style="width: 300px;right:0px" class="form-control input-item"><br>'/*+ ' - ' + o[i]*/);
+                          }
+                          /*else{
+                              k.push(name + i +'<br>'/!*+ ' - ' + o[i]*!/);
+                          }*/
+                      }
+
+                  }
+              }
+          })
+          (adsJson);
+        /*var div = document.createElement('div');
+        div.innerHTML=k.join(" ");*/
+        document.getElementById('form').innerHTML=k.join(" ");
+    }
+
+   //change dataJson
     function setNewDataJson(inputId) {
         var val=document.getElementById(inputId).value;
         inputId=inputId.toString().replace(/\s+/g, "");
@@ -291,17 +326,22 @@
                 }
                 else{
                    if((name+i).indexOf(inputId)!=-1){
-                       console.log("tim thay");
+                       //console.log("tim thay");
                        o[i]=val;
                    }
-                   else console.log("ko tim thay");
+                   //else console.log("ko tim thay");
                 }
             }
         })
         (adsJson);
-
         document.getElementById('scriptText').value=adsScript.replace(adsScript.slice(adsScript.indexOf("{"),adsScript.lastIndexOf("}")+1),JSON.stringify(adsJson));
+    }
 
+    //remove remove-duplicate-values-from dataJson
+    function remove_duplicates(arr) {
+        let s = new Set(arr);
+        let it = s.values();
+        return Array.from(it);
     }
 
     // view script text
